5a1d36e692bd53b033cae46ebe05c898ab212104
aren0416 <aren0416@gmail.com>
2022-05-30 18:39:09 +0900

    router setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { routes } from "./routes";
+import { Home } from "./screens/Home";
+import { NotFound } from "./screens/NotFound";
+import { SignUp } from "./screens/SignUp";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.signUp} element={<SignUp />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
